6478acc49352abf6221bdd7c844207ce0967f2ca
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-12-21 17:27:42 -0300

    first commit + rakic98 dl model

--- goodasgold/examples/gold_plt.py
+++ goodasgold/examples/gold_plt.py
@@ -7,12 +7,17 @@ from goodasgold.convert import nk_from_er
 if __name__ == "__main__":
 
     fig = plt.figure()
-    ax = plt.axes()
 
     wl = np.linspace(0.2, 1.5, 100)
     er = [rakic98.au_dl(w) for w in wl]
     n = [nk_from_er(e[0], e[1])[0] for e in er]
     k = [nk_from_er(e[0], e[1])[1] for e in er]
-    ax.plot(wl, n)
-    ax.plot(wl, k)
+
+    plt.plot(wl, n, label="n")
+    plt.plot(wl, k, label="k")
+    plt.xlabel('Wavelength (μm)')
+    plt.ylabel('n, k')
+
+    plt.legend(loc="upper left")
+
     plt.show()
